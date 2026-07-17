b91a3a98133fb9680e815b4b46d5fc547ec4fd93
alexalves011 <alexalves.work@gmail.com>
2026-07-17 19:42:27 -0300

    Meu primeiro commit

---
+++ src/Store/Slices/taskSlice.js
@@ -0,0 +1,53 @@
+// responsavel por gerenciar tudo em referente a TASK
+
+import { createSlice, isAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      const taskWithId = {
+        ...action.payload,
+        id: Date.now(),
+        completed: false,
+      };
+      state.tasks.push(taskWithId);
+    },
+
+    toggleTaskComplete: (state, action) => {
+      const task = state.tasks.find((task) => task.id === action.payload);
+      if (task) task.completed = !task.completed;
+    },
+
+    editTask: (state, action) => {
+      const { taskId, updatedTask } = action.payload;
+      const taskIndex = state.tasks.findIndex((task) => task.id === taskId);
+
+      if (taskIndex !== -1) {
+        // Atribuímos o novo objeto diretamente à posição correta da lista
+        state.tasks[taskIndex] = {
+          ...state.tasks[taskIndex], // Mantém os dados antigos (como o 'id')
+          ...updatedTask, // Sobrescreve com os dados novos da edição
+        };
+      }
+    },
+
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+  },
+});
+
+export const { addTask, toggleTaskComplete, editTask, deleteTask } =
+  taskSlice.actions;
+
+export const selectTasks = (state) => state.task.tasks;
+export const selectPendingTasks = (state) => state.tasks.tasks.filter(task => !task.completed)
+export const selectCompletedTasks = (state) => state.tasks.tasks.filter(task => task.completed)
+
+export default taskSlice.reducer
